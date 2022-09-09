d531f3f1fa9b5429117c9ea3441231c5636cf439
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-09-09 11:29:18 -0700

    Question system working. Score and name logged

--- script.js
+++ script.js
@@ -2,64 +2,154 @@
 var startContainer = document.querySelector('.start-container');
 var startQuizBtn = document.querySelector('.start-btn');
 var quizContainer = document.querySelector('.quiz-container');
+var gameOverContainer = document.querySelector('.game-over-container');
+var highScoreContainer = document.querySelector('.high-score-container');
+var highScoreBtn = document.querySelector('.high-score-btn');
 var myQuestions = [
     {
-        question: "This is a question?",
+        question: "This is question id 1?",
         answers: {
             1: "Answer a",
             2: "Answer b",
             3: "Answer c",
-            4: "Answer d"
+            4: "Answer d",
         },
-        correctAnswer: 4
+        correctAnswer: 4,
+        questionID: 1
     },
     {
-        question: "This is another question?",
+        question: "This is question id 2?",
         answers: {
-            1: "Another Answer a",
-            2: "Another Answer b",
-            3: "Another Answer c",
-            4: "Another Answer d"
+            1: "Answer a",
+            2: "Answer b",
+            3: "Answer c",
+            4: "Answer d",
         },
-        correctAnswer: 1
+        correctAnswer: 4,
+        questionID: 2
+    },
+    {
+        question: "This is question id 3?",
+        answers: {
+            1: "Answer a",
+            2: "Answer b",
+            3: "Answer c",
+            4: "Answer d",
+        },
+        correctAnswer: 4,
+        questionID: 3
     },
 ]
-
+var askedQuestions = [];
+var questionsToAsk = [];
+var randomQuestion;
+var correctAnswers = 0;
+var userScoreName = "";
 
 startQuizBtn.addEventListener('click', startQuiz);
+init();
 
 function startQuiz() {
     startContainer.setAttribute('style', 'display: none');
     quizContainer.setAttribute('style', 'display: flex');
-    createQuestion(getRandomQuestion());
+    correctAnswers = 0;
+    getRandomQuestion();
+    createQuestion();
+}
+
+function init() {
+    for (i = 0; i < myQuestions.length; i++) {
+    questionsToAsk.push(myQuestions[i].questionID);
+    }
 }
 
 function getRandomQuestion() {
     var randomIndex = Math.floor(Math.random() * myQuestions.length);
-    return myQuestions[randomIndex];
+    randomQuestion = myQuestions[randomIndex];
+
+    if (askedQuestions.includes(randomQuestion.questionID)){
+        if (questionsToAsk.length === 0) {
+            gameOver();
+        } else {
+            getRandomQuestion();
+        }
+    } else {
+        askedQuestions.push(randomQuestion.questionID);
+        questionToRemove = questionsToAsk.indexOf(randomQuestion.questionID);
+        questionsToAsk.splice(questionToRemove, 1);
+    }
 }
 
-function createQuestion(randomQuestionIndex) {
+function createQuestion() {
      var question = document.createElement('h2')
-     question.innerText = randomQuestionIndex.question;
+     question.innerText = randomQuestion.question;
      quizContainer.appendChild(question);
 
      var answerList = document.createElement('ol');
      quizContainer.appendChild(answerList);
 
-     for (i = 1; i <= Object.values(randomQuestionIndex.answers).length; i++) {
-        if (Object.keys(randomQuestionIndex.answers).find(correctAnswer => i === randomQuestionIndex.correctAnswer)) {
+     for (i = 1; i <= Object.values(randomQuestion.answers).length; i++) {
+        if (Object.keys(randomQuestion.answers).find(correctAnswer => i === randomQuestion.correctAnswer)) {
             var answer = document.createElement('li');
-            answer.innerHTML = "<button class='ans-btn correct'>" + randomQuestionIndex.answers[i] + "</button>"
+            answer.innerHTML = "<button class='ans-btn' id='correct'>" + randomQuestion.answers[i] + "</button>"
             answerList.appendChild(answer);
+            answer.addEventListener('click', checkAnswer)
         } else {
             var answer = document.createElement('li');
-            answer.innerHTML = "<button class=ans-btn>" + randomQuestionIndex.answers[i] + "</button>"
+            answer.innerHTML = "<button class=ans-btn>" + randomQuestion.answers[i] + "</button>"
             answerList.appendChild(answer);
+            answer.addEventListener('click', checkAnswer)
         }
      }
 }
 
-function checkAnswer() {
+function checkAnswer(event) {
+    var selectedAnswer = event.target;
+    var correct = selectedAnswer.getAttribute('id')
+    if (correct) {
+        correctAnswers++;
+        resetQuestion();
+        getRandomQuestion();
+        createQuestion();
+    } else {
+        resetQuestion();
+        getRandomQuestion();
+        createQuestion();
+    }
+}
 
+function resetQuestion() {
+    for (i = 0; i <= quizContainer.childElementCount; i++)
+    quizContainer.lastElementChild.remove();
+}
+
+function gameOver() {
+    quizContainer.setAttribute('style', 'display: none');
+    gameOverContainer.setAttribute('style', 'display: flex');
+
+    var gameOverHeader = document.createElement('h2');
+    gameOverHeader.innerText = "GAME OVER";
+    gameOverContainer.appendChild(gameOverHeader);
+
+    var form = document.createElement('form');
+    form.setAttribute('action', '#');
+    gameOverContainer.appendChild(form);
+
+    var nameInput = document.createElement('input');
+    nameInput.setAttribute('type', 'name');
+    nameInput.setAttribute('name', 'name');
+    nameInput.setAttribute('placeholder', 'Enter your name...');
+    form.appendChild(nameInput);
+
+    var submitScoreBtn = document.createElement('button');
+    submitScoreBtn.textContent = "Sumbit";
+    form.appendChild(submitScoreBtn);
+    submitScoreBtn.addEventListener('click', submitScore);
+
+    function submitScore(event) {
+        event.preventDefault();
+        userScoreName = nameInput.value;
+        console.log(userScoreName);
+        console.log(correctAnswers);
+    }
 }
